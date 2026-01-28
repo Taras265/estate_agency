0647ca63d38eb6f5a82967284290f42e322296b4
Taras Bialyk <taras.bialyk@gmail.com>
2026-01-28 11:53:11 +0200

    tag querystring

--- objects/templatetags/objects_tags.py
+++ objects/templatetags/objects_tags.py
@@ -1,6 +1,7 @@
 from typing import TypeVar
 
 from django import template
+from django.utils.html import format_html
 
 K = TypeVar("K")
 V = TypeVar("V")
@@ -16,3 +17,17 @@ def get_dict_value(dictionary: dict[K, V], key: K) -> V | None:
 @register.simple_tag
 def get_model_name(obj):
     return obj.__class__.__name__.lower()
+
+
+@register.simple_tag(takes_context=True)
+def next_sort_direction(context, field):
+    """
+    Обчислює якою має бути сортировка
+    """
+    request = context['request']
+    current_sort = request.GET.get('sort')
+    current_direction = request.GET.get('direction', 's')
+
+    if current_sort == field:
+        return 'd' if current_direction == 's' else 's'
+    return 's'
